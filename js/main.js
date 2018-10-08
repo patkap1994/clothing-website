13258e834d9e4270ccf229aff2a9844230f77fe5
Patryk <patryk.kapkowski@gmail.com>
2018-10-08 20:57:12 +0200

    Added owl carousel

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ function toggleMenu() {
     navMenu.classList.toggle('active');
     this.classList.toggle('active');
 }
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 100,
+        items: 3,
+        responsive : {
+            0 : {
+                items: 1
+            },
+            480 : {
+                items: 2
+            },
+            768 : {
+                items: 3
+            },
+            1000 : {
+                items: 4
+            }
+        }
+    });
+  });
