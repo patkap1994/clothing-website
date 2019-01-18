23c23347d56446a5d24f327fd9c2d75e0f626358
Patryk <patryk.kapkowski@gmail.com>
2019-01-18 15:28:40 +0100

    Made some optimization changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,25 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat-css');
-const imagemin = require('gulp-imagemin');
 const concatJS = require('gulp-concat');
+const minifyJS = require('gulp-minify');
+const cleanCSS = require('gulp-clean-css');
 
 //Concatenation of all css files into one css file
-gulp.task('concat', function() {
+gulp.task('buildCSS', function() {
     return gulp.src('./src/css/**/*.css')
     .pipe(concat('bundle.css'))
+    .pipe(cleanCSS())
     .pipe(gulp.dest('dist/css/'));
 });
 
-//Minifying images
-gulp.task('minifyimg', function() {
-    return gulp.src('./src/img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img/'));
-});
-
 //Concatenation of JS files
-//Minifying images
-gulp.task('concatJS', function() {
+gulp.task('buildJS', function() {
     return gulp.src('./src/js/**/*.js')
     .pipe(concatJS('bundle.js'))
+    .pipe(minifyJS())
     .pipe(gulp.dest('dist/js/'));
 });
 
 //All above
-gulp.task('build', ['concat', 'minifyimg', 'concatJS']);
+gulp.task('build', ['buildCSS', 'buildJS']);
 
